9076d7552308c200009e0325ebce29b579efe903
jpcchaves <jpcchaves@outlook.com>
2024-09-02 10:09:04 -0300

    add role repository

---
+++ src/main/java/com/finances/finances/persistence/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.finances.finances.persistence.repository;
+
+import com.finances.finances.domain.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {}
