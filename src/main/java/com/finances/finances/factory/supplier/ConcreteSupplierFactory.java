c3c1b3663234644c7f0db111fa6b757cff27d8bb
jpcchaves <jpcchaves@outlook.com>
2024-09-06 09:11:57 -0300

    add supplier factory

---
+++ src/main/java/com/finances/finances/factory/supplier/ConcreteSupplierFactory.java
@@ -0,0 +1,28 @@
+package com.finances.finances.factory.supplier;
+
+import com.finances.finances.domain.dto.supplier.SupplierResponseDTO;
+import com.finances.finances.domain.entities.Supplier;
+import com.finances.finances.domain.entities.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConcreteSupplierFactory implements SupplierFactory {
+
+  @Override
+  public SupplierResponseDTO buildSupplierResponseDTO(Supplier supplier) {
+
+    return new SupplierResponseDTO(supplier.getId(), supplier.getName());
+  }
+
+  @Override
+  public SupplierResponseDTO buildSupplierResponseDTO(Long supplierId, String name) {
+
+    return new SupplierResponseDTO(supplierId, name);
+  }
+
+  @Override
+  public Supplier buildSupplier(Long supplierId, String name, User user) {
+
+    return new Supplier(supplierId, name, user);
+  }
+}
---
+++ src/main/java/com/finances/finances/factory/supplier/SupplierFactory.java
@@ -0,0 +1,14 @@
+package com.finances.finances.factory.supplier;
+
+import com.finances.finances.domain.dto.supplier.SupplierResponseDTO;
+import com.finances.finances.domain.entities.Supplier;
+import com.finances.finances.domain.entities.User;
+
+public interface SupplierFactory {
+
+  SupplierResponseDTO buildSupplierResponseDTO(Supplier supplier);
+
+  SupplierResponseDTO buildSupplierResponseDTO(Long supplierId, String name);
+
+  Supplier buildSupplier(Long supplierId, String name, User user);
+}
