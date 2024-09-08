c967ecfe6da6ce89068d9993bb7939e2cab4609c
jpcchaves <jpcchaves@outlook.com>
2024-09-08 08:46:56 -0300

    add expense mapper

---
+++ src/main/java/com/finances/finances/mapper/expense/ExpenseMapper.java
@@ -0,0 +1,22 @@
+package com.finances.finances.mapper.expense;
+
+import com.finances.finances.domain.dto.expense.ExpenseResponseDTO;
+import com.finances.finances.domain.entities.Expense;
+import com.finances.finances.util.mapper.MapperUtil;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExpenseMapper {
+
+  private final MapperUtil mapperUtil;
+
+  public ExpenseMapper(MapperUtil mapperUtil) {
+    this.mapperUtil = mapperUtil;
+  }
+
+  public List<ExpenseResponseDTO> toDTO(List<Expense> expenseList) {
+
+    return mapperUtil.mapObjects(expenseList, ExpenseResponseDTO.class);
+  }
+}
