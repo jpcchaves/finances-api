eff0242f459d1f0eb7c65dfd41c1a25f152e72e2
jpcchaves <jpcchaves@outlook.com>
2024-09-12 10:18:56 -0300

    adjust swagger doc

--- src/main/java/com/finances/finances/controller/ExpenseController.java
+++ src/main/java/com/finances/finances/controller/ExpenseController.java
@@ -4,16 +4,77 @@ import com.finances.finances.domain.dto.common.PaginationResponseDTO;
 import com.finances.finances.domain.dto.common.ResponseDTO;
 import com.finances.finances.domain.dto.expense.ExpenseRequestDTO;
 import com.finances.finances.domain.dto.expense.ExpenseResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springdoc.core.converters.models.PageableAsQueryParam;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Expense Controller")
+@SecurityRequirement(name = "Bearer Authentication")
 public interface ExpenseController {
 
+  @Operation(
+      summary = "Creates a new expense",
+      description = "Creates a new expense by passing a JSON representation of ExpenseRequestDTO",
+      responses = {
+        @ApiResponse(
+            description = "Created",
+            responseCode = "201",
+            content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
+        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+        @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+      })
   ResponseEntity<ResponseDTO<?>> create(ExpenseRequestDTO requestDTO);
 
+  @Operation(
+      summary = "Updates a expense",
+      description =
+          "Updates a expense by passing a expense ID and  a JSON representation of ExpenseRequestDTO",
+      responses = {
+        @ApiResponse(
+            description = "Success",
+            responseCode = "200",
+            content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
+        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+        @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+      })
   ResponseEntity<ResponseDTO<?>> update(Long expenseId, ExpenseRequestDTO requestDTO);
 
-  ResponseEntity<ResponseDTO<PaginationResponseDTO<ExpenseResponseDTO>>> list(Pageable pageable);
+  @Operation(
+      summary = "Gets expense list",
+      description = "Gets expense list related to user",
+      responses = {
+        @ApiResponse(
+            description = "Success",
+            responseCode = "200",
+            content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
+        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+        @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+      })
+  @PageableAsQueryParam
+  ResponseEntity<ResponseDTO<PaginationResponseDTO<ExpenseResponseDTO>>> list(
+      @Parameter(hidden = true) Pageable pageable);
 
+  @Operation(
+      summary = "Gets a expense by id",
+      description = "Gets a expense by id by passing a expense id as a path variable",
+      responses = {
+        @ApiResponse(
+            description = "Success",
+            responseCode = "200",
+            content = @Content(schema = @Schema(implementation = ExpenseResponseDTO.class))),
+        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+        @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+      })
   ResponseEntity<ResponseDTO<ExpenseResponseDTO>> findById(Long expenseId);
 }
--- src/main/java/com/finances/finances/controller/SupplierController.java
+++ src/main/java/com/finances/finances/controller/SupplierController.java
@@ -4,15 +4,77 @@ import com.finances.finances.domain.dto.common.PaginationResponseDTO;
 import com.finances.finances.domain.dto.common.ResponseDTO;
 import com.finances.finances.domain.dto.supplier.SupplierRequestDTO;
 import com.finances.finances.domain.dto.supplier.SupplierResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springdoc.core.converters.models.PageableAsQueryParam;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Supplier Controller")
+@SecurityRequirement(name = "Bearer Authentication")
 public interface SupplierController {
+
+  @Operation(
+      summary = "Creates a new supplier",
+      description = "Creates a new supplier by passing a JSON representation of SupplierRequestDTO",
+      responses = {
+        @ApiResponse(
+            description = "Created",
+            responseCode = "201",
+            content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
+        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+        @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+      })
   ResponseEntity<ResponseDTO<?>> create(SupplierRequestDTO requestDTO);
 
+  @Operation(
+      summary = "Updates a supplier",
+      description =
+          "Updates a supplier by passing a supplier ID and  a JSON representation of SupplierRequestDTO",
+      responses = {
+        @ApiResponse(
+            description = "Success",
+            responseCode = "200",
+            content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
+        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+        @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+      })
   ResponseEntity<ResponseDTO<?>> update(Long supplierId, SupplierRequestDTO requestDTO);
 
-  ResponseEntity<ResponseDTO<PaginationResponseDTO<SupplierResponseDTO>>> list(Pageable pageable);
+  @Operation(
+      summary = "Gets supplier list",
+      description = "Gets supplier list related to user",
+      responses = {
+        @ApiResponse(
+            description = "Success",
+            responseCode = "200",
+            content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
+        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+        @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+      })
+  @PageableAsQueryParam
+  ResponseEntity<ResponseDTO<PaginationResponseDTO<SupplierResponseDTO>>> list(
+      @Parameter(hidden = true) Pageable pageable);
 
+  @Operation(
+      summary = "Gets a supplier by id",
+      description = "Gets a supplier by id by passing a supplier id as a path variable",
+      responses = {
+        @ApiResponse(
+            description = "Success",
+            responseCode = "200",
+            content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
+        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+        @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+      })
   ResponseEntity<ResponseDTO<SupplierResponseDTO>> findById(Long supplierId);
 }
