6aa40bff44ebb69b9d20a44f03766b94dff2a40b
jpcchaves <jpcchaves@outlook.com>
2024-09-13 09:52:06 -0300

    add migration init

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,400 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.8 (Debian 15.8-1.pgdg120+1)
+-- Dumped by pg_dump version 15.8 (Debian 15.8-1.pgdg120+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: expenses; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.expenses (
+    id bigint NOT NULL,
+    amount numeric(38,2) NOT NULL,
+    created_at timestamp(6) without time zone NOT NULL,
+    deleted_at timestamp(6) without time zone,
+    description character varying(255) NOT NULL,
+    due_date date NOT NULL,
+    notes character varying(255),
+    updated_at timestamp(6) without time zone NOT NULL,
+    financial_category_id bigint NOT NULL,
+    supplier_id bigint NOT NULL,
+    user_id bigint NOT NULL
+);
+
+
+ALTER TABLE public.expenses OWNER TO postgres;
+
+--
+-- Name: financial_categories; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.financial_categories (
+    id bigint NOT NULL,
+    name character varying(100) NOT NULL,
+    user_id bigint NOT NULL,
+    created_at timestamp(6) without time zone,
+    updated_at timestamp(6) without time zone
+);
+
+
+ALTER TABLE public.financial_categories OWNER TO postgres;
+
+--
+-- Name: roles; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.roles (
+    id bigint NOT NULL,
+    name character varying(100) NOT NULL
+);
+
+
+ALTER TABLE public.roles OWNER TO postgres;
+
+--
+-- Name: seq_expense; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.seq_expense
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.seq_expense OWNER TO postgres;
+
+--
+-- Name: seq_financial_category; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.seq_financial_category
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.seq_financial_category OWNER TO postgres;
+
+--
+-- Name: seq_role; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.seq_role
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.seq_role OWNER TO postgres;
+
+--
+-- Name: seq_supplier; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.seq_supplier
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.seq_supplier OWNER TO postgres;
+
+--
+-- Name: seq_user; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.seq_user
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.seq_user OWNER TO postgres;
+
+--
+-- Name: suppliers; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.suppliers (
+    id bigint NOT NULL,
+    name character varying(255) NOT NULL,
+    user_id bigint NOT NULL,
+    created_at timestamp(6) without time zone,
+    updated_at timestamp(6) without time zone
+);
+
+
+ALTER TABLE public.suppliers OWNER TO postgres;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id bigint NOT NULL,
+    created_at timestamp(6) without time zone,
+    email character varying(150) NOT NULL,
+    name character varying(200) NOT NULL,
+    password character varying(255) NOT NULL,
+    updated_at timestamp(6) without time zone
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: users_roles; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users_roles (
+    user_id bigint NOT NULL,
+    role_id bigint NOT NULL
+);
+
+
+ALTER TABLE public.users_roles OWNER TO postgres;
+
+--
+-- Data for Name: expenses; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.expenses VALUES (1, 217.39, '2024-09-12 11:15:54.184', NULL, 'Feira semanal setembro', '2024-09-07', NULL, '2024-09-12 11:15:54.184', 1, 1, 1);
+
+
+--
+-- Data for Name: financial_categories; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.financial_categories VALUES (1, 'Alimentação', 1, '2024-09-12 11:08:52.106004', '2024-09-12 11:08:52.106061');
+
+
+--
+-- Data for Name: roles; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.roles VALUES (1, 'ROLE_USER');
+
+
+--
+-- Data for Name: suppliers; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.suppliers VALUES (1, 'Assai Atacadista', 1, '2024-09-12 11:08:24.07951', '2024-09-12 11:08:24.079569');
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.users VALUES (1, '2024-09-12 11:04:50.739464', 'jpcchaves@outlook.com', 'João Paulo', '$2a$10$/CYt/C5.q8QWr67iKMnqMO9Nh1OFCo6sa0Ri0AZP/R1Nw/go5rVTm', '2024-09-12 11:04:50.739629');
+
+
+--
+-- Data for Name: users_roles; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.users_roles VALUES (1, 1);
+
+
+--
+-- Name: seq_expense; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.seq_expense', 1, true);
+
+
+--
+-- Name: seq_financial_category; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.seq_financial_category', 1, true);
+
+
+--
+-- Name: seq_role; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.seq_role', 1, true);
+
+
+--
+-- Name: seq_supplier; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.seq_supplier', 1, true);
+
+
+--
+-- Name: seq_user; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.seq_user', 1, true);
+
+
+--
+-- Name: expenses expenses_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.expenses
+    ADD CONSTRAINT expenses_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: financial_categories financial_categories_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.financial_categories
+    ADD CONSTRAINT financial_categories_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: roles roles_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.roles
+    ADD CONSTRAINT roles_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: suppliers suppliers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.suppliers
+    ADD CONSTRAINT suppliers_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: financial_categories unique_category_name; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.financial_categories
+    ADD CONSTRAINT unique_category_name UNIQUE (name);
+
+
+--
+-- Name: users unique_email; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT unique_email UNIQUE (email);
+
+
+--
+-- Name: roles unique_role_name; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.roles
+    ADD CONSTRAINT unique_role_name UNIQUE (name);
+
+
+--
+-- Name: suppliers unique_supplier_name; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.suppliers
+    ADD CONSTRAINT unique_supplier_name UNIQUE (name);
+
+
+--
+-- Name: users_roles unique_user_role; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users_roles
+    ADD CONSTRAINT unique_user_role PRIMARY KEY (user_id, role_id);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: expenses financial_category_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.expenses
+    ADD CONSTRAINT financial_category_fk FOREIGN KEY (financial_category_id) REFERENCES public.financial_categories(id);
+
+
+--
+-- Name: users_roles role_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users_roles
+    ADD CONSTRAINT role_fk FOREIGN KEY (role_id) REFERENCES public.roles(id);
+
+
+--
+-- Name: expenses supplier_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.expenses
+    ADD CONSTRAINT supplier_fk FOREIGN KEY (supplier_id) REFERENCES public.suppliers(id);
+
+
+--
+-- Name: expenses user_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.expenses
+    ADD CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: financial_categories user_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.financial_categories
+    ADD CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: suppliers user_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.suppliers
+    ADD CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: users_roles user_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users_roles
+    ADD CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
