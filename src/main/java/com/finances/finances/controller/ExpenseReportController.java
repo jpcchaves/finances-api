450789305183e0972cf2a148e9233aa0190c4d57
jpcchaves <jpcchaves@outlook.com>
2024-09-16 22:14:18 -0300

    add expense controller tag and security scheme swagger

--- src/main/java/com/finances/finances/controller/ExpenseReportController.java
+++ src/main/java/com/finances/finances/controller/ExpenseReportController.java
@@ -3,11 +3,14 @@ package com.finances.finances.controller;
 import com.finances.finances.domain.dto.common.ExpenseGroupedByCategoryDTO;
 import com.finances.finances.domain.dto.common.ExpenseGroupedBySupplierDTO;
 import com.finances.finances.domain.dto.common.ResponseDTO;
-import org.springframework.http.ResponseEntity;
-
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.time.LocalDate;
 import java.util.List;
+import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Expense Report Controller")
+@SecurityRequirement(name = "Bearer Authentication")
 public interface ExpenseReportController {
 
   ResponseEntity<ResponseDTO<List<ExpenseGroupedByCategoryDTO>>> getExpensesGroupedByAllCategories(
